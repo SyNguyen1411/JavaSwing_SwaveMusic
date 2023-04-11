4bfdc93cdaa0220c95d6faef53d3bb07dc3baa98
Admin <Admin@PhanQuiDuc>
2023-04-11 23:23:22 +0700

    11/4/2023 ver3 Hoàn thiện PlaylistDAO

--- src/dao/PlaylistDAO.java
+++ src/dao/PlaylistDAO.java
@@ -5,48 +5,72 @@
 package dao;
 
 import entity.PlayList;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import utils.JdbcHelper;
 
 /**
  *
  * @author Admin
  */
 public class PlaylistDAO extends SwaveDAO<PlayList, Integer> {
-    // Chưa hoàn thiện
-    final String INSERT_SQL = "INSERT INTO BAIHAT(TenBH, NhacSi, CaSi, TheLoai, LoiBH, HinhAnh, FileBH, TrangThai, MaND) VALUES (?,?,?,?,?,?,?,?,?)";
-    final String UPDATE_SQL = "UPDATE BAIHAT SET TenBH = ?, NhacSi = ?, CaSi = ?, TheLoai = ?, LoiBH = ?, HinhAnh = ?, FileBH = ?, TrangThai = ?, WHERE MaBH = ?";
-    final String DELETE_SQL = "DELETE FROM BAIHAT WHERE MaBH = ?";
-    final String SELECTALL_SQL = "SELECT * FROM BAIHAT";
-    final String SELECTBYID_SQL = "SELECT * FROM BAIHAT WHERE MaBH = ?";
-    
+
+    final String INSERT_SQL = "INSERT INTO PLAYLIST(TenPlayList, RiengTu, HinhAnh, MaND) VALUES (?,?,?,?)";
+    final String UPDATE_SQL = "UPDATE PLAYLIST SET TenPlayList = ?, RiengTu = ?, HinhAnh = ? WHERE MaPlayList = ?";
+    final String DELETE_SQL = "DELETE FROM PLAYLIST WHERE MaPlayList = ?";
+    final String SELECTALL_SQL = "SELECT * FROM PLAYLIST";
+    final String SELECTBYID_SQL = "SELECT * FROM PLAYLIST WHERE MaPlayList = ?";
+
     @Override
     public void insert(PlayList entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        JdbcHelper.update(INSERT_SQL, entity.getPlaylistName(), entity.isStatus(),
+                entity.getIcon(), entity.getUserID());
     }
 
     @Override
     public void update(PlayList entity) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        JdbcHelper.update(INSERT_SQL, entity.getPlaylistName(), entity.isStatus(),
+                entity.getIcon(), entity.getPlaylistID());
     }
 
     @Override
     public void delete(Integer key) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        JdbcHelper.update(DELETE_SQL, key);
     }
 
     @Override
     public List<PlayList> selectAll() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return selectSql(SELECTALL_SQL);
     }
 
     @Override
     public PlayList selectById(Integer key) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<PlayList> list = selectSql(SELECTBYID_SQL, key);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
     }
 
     @Override
     public List<PlayList> selectSql(String Sql, Object... args) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<PlayList> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(Sql, args);
+            while (rs.next()) {
+                PlayList entity = new PlayList();
+                entity.setPlaylistID(rs.getInt(1));
+                entity.setPlaylistName(rs.getString(2));
+                entity.setStatus(rs.getBoolean(3));
+                entity.setIcon(rs.getString(4));
+                entity.setUserID(rs.getInt(5));
+                list.add(entity);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return list;
     }
-    
+
 }
